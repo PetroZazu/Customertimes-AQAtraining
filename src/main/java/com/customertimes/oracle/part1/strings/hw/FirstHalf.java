614e8ae43cae3cd9d87c70c17bb6609503f9b03e
Petro Zazuliak <petro.zazuliak@ctdev.io>
2020-04-12 13:18:51 +0300

    subString method works

---
+++ src/main/java/com/customertimes/oracle/part1/strings/hw/FirstHalf.java
@@ -0,0 +1,18 @@
+package com.customertimes.oracle.part1.strings.hw;
+/*
+Given a string of even length, return the first half. So the string "WooHoo" yields "Woo".
+
+
+firstHalf("WooHoo") → "Woo"
+firstHalf("HelloThere") → "Hello"
+firstHalf("abcdef") → "abc"
+ */
+public class FirstHalf {
+    public static String firstHalfOfTheString(String str){
+        return str.substring(0, str.length()/2);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(firstHalfOfTheString("onetwothre"));
+    }
+}
